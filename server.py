21ed63c3c294a9e514d1bfa4f19d14cfc3f42fd2
acbk2b <acbecker27@gmail.com>
2020-10-07 14:38:42 -0500

    Added basic flask app

---
+++ server.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/quiz")
+def quiz():
+    return render_template("quiz.html")
+
+@app.route("/data")
+def data():
+    return render_template("data.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
