d465b8d0f4bccad5ba8ab923034535b8ac53f30d
Catherine Sauer <cathyrae903@gmail.com>
2020-11-19 15:21:39 -0600

    Add bar graph for trait distribution

--- server.py
+++ server.py
@@ -1,5 +1,10 @@
-from flask import Blueprint, Flask, render_template, request
+from flask import Blueprint, Flask, render_template, request, Response
 import sqlite3
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+import pandas as pd
+import collections
+import io
 app = Flask(__name__)
 
 NUM_QUESTIONS = 50
@@ -8,6 +13,78 @@ def get_db_connection():
     conn = sqlite3.connect('database.db')
     conn.row_factory = sqlite3.Row
     return conn
+
+@app.route('/plot.png')
+def plot_png():
+    fig = update_summary_graphs()
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
+
+def update_summary_graphs():
+    # Retrieve all submission data from database.
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM submissions').fetchall()
+    conn.close()
+
+    # Data currently stored in two different methods for data calculations.
+    summary = {
+        'Extrovert': 0,
+        'Introvert': 0,
+        'Sensor': 0,
+        'Intuitive': 0,
+        'Thinker': 0,
+        'Feeler': 0,
+        'Judger': 0,
+        'Perceiver': 0,
+    }
+    df = pd.DataFrame(columns=['Extrovert', 'Introvert', 'Sensor', 'Intuitive', 'Thinker', 'Feeler', 'Judger', 'Perceiver'])
+
+    # Store aggregate database data.
+    for i in range(0, len(posts)):
+        summary['Extrovert'] += posts[i]['sumE']
+        summary['Introvert'] += posts[i]['sumI']
+        summary['Sensor'] += posts[i]['sumS']
+        summary['Intuitive'] += posts[i]['sumN']
+        summary['Thinker'] += posts[i]['sumT']
+        summary['Feeler'] += posts[i]['sumF']
+        summary['Judger'] += posts[i]['sumJ']
+        summary['Perceiver'] += posts[i]['sumP']
+        new_row = { 'Extrovert': posts[i]['sumE'],
+                    'Introvert': posts[i]['sumI'],
+                    'Sensor':    posts[i]['sumS'],
+                    'Intuitive': posts[i]['sumN'],
+                    'Thinker':   posts[i]['sumT'],
+                    'Feeler':    posts[i]['sumF'],
+                    'Judger':    posts[i]['sumJ'],
+                    'Perceiver': posts[i]['sumP'] }
+        df = df.append(new_row, ignore_index=True)
+
+    # Sort trait sums in ascending order.
+    summary = collections.OrderedDict(sorted(summary.items(), key=lambda kv: kv[1]))
+
+    # Generate `Trait Distribution` graph.
+    colors = []
+    for val in summary.keys():
+        if val == 'Extrovert' or val == 'Introvert':
+            colors.append('blue')
+        elif val == 'Sensor' or val == 'Intuitive':
+            colors.append('green')
+        elif val == 'Thinker' or val == 'Feeler':
+            colors.append('red')
+        elif val == 'Judger' or val == 'Perceiver':
+            colors.append('orange')
+    fig = Figure(figsize=(10, 10))
+    ax = fig.add_subplot(1, 1, 1)
+    ax.bar(summary.keys(), summary.values(), width=0.8, color=colors)
+    ax.set_xlabel('Trait', fontsize=14)
+    ax.set_ylabel('Total Trait Sum', fontsize=14)
+    ax.tick_params(axis='both', which='major', labelsize=13)
+    fig.suptitle('Individual Trait Distribution', fontsize=16)
+    fig.tight_layout()
+    fig.savefig('./assets/bar.png', dpi=300, bbox_inches='tight', transparent=False)
+
+    return fig
 
 @app.route("/")
 def home():
